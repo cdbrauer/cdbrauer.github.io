d6057fb67747e763f6610ade7854bf01a08bb163
cdbrauer <cole.brauer@outlook.com>
2020-07-02 14:06:07 -0700

    Added links

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 // Toggle Main Menu
 function openMenu() {
-    $("#main-menu").slideDown(500);
+    $("#main-menu").slideDown(300);
     $("#menu-open").hide();
     $("#menu-close").show();
 }
 
 function closeMenu() {
-    $("#main-menu").slideUp(500);
+    $("#main-menu").slideUp(300);
     $("#menu-close").hide();
     $("#menu-open").show();
 }
+
+$(document).click(function (event) {
+    if (!$(event.target).closest('#main-menu').length) {
+        if ($('#main-menu').is(":visible")) {
+            closeMenu();
+        }
+    }
+})
