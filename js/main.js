3d7dbaf7c33815b5471e8b5bfda1d9e2f47de168
cdbrauer <cole.brauer@outlook.com>
2020-07-03 11:30:04 -0700

    Initial version of a sub-page sidebar

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ function openMenu() {
     $("#main-menu").slideDown(300);
     $("#menu-open").hide();
     $("#menu-close").show();
+    $("#sidebar").animate({opacity: '0'}, 300);
 }
 
 function closeMenu() {
     $("#main-menu").slideUp(300);
     $("#menu-close").hide();
     $("#menu-open").show();
+    $("#sidebar").animate({opacity: '1'}, 300);
 }
 
 $(document).click(function (event) {
@@ -18,3 +20,25 @@ $(document).click(function (event) {
         }
     }
 })
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const $sidebar = $("#sidebar");
+    const rect = document.getElementById("sidebar-container").getBoundingClientRect();
+    const currentOpacity = $sidebar.css("opacity")
+
+    console.log(currentOpacity);
+
+    if (rect.top < 0) {
+        // if (currentOpacity == 0){
+        //     $sidebar.animate({opacity: '1'}, 300);
+        // }
+        document.getElementById("sidebar").style.top = -rect.top + "px";
+    } else {
+        // if (currentOpacity == 1) {
+        //     $sidebar.animate({opacity: '0'}, 300);
+        // }
+        document.getElementById("sidebar").style.top = "0";
+    }
+}
